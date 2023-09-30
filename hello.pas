2e40e31d7aaec5b44a8d7f27e9d9b79c6fc2925f
mafrolo@sfedu.ru <mafrolo@sfedu.ru>
2023-09-30 08:30:36 +0300

    hello

--- hello.pas
+++ hello.pas
@@ -1,2 +1,7 @@
 ﻿##
-print('Привет, мир!')
+var N := ReadInteger('Который час?');
+assert(N in 0..23);
+if N in 4..10 then print('Доброе утро, мир!')
+else if N in 11..16 then print('Добрый день, мир!')
+else if N in 17..22 then print('Добрый вечер, мир!')
+else print('Доброй ночи, мир!')
